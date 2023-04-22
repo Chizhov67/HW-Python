1abb91e2e5c3cade2ce517fdd609c5a5f8b4e649
Andrey <chizhov67@gmail.com>
2023-04-22 16:51:28 +0300

    task_2

---
+++ HW4/task_2.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list()
+for i in range(n):
+    х = int(input())
+    arr.apend(x)
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
